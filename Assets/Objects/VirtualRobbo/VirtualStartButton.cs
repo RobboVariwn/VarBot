cb389a74e7ff3caab557a6f655b007b123af8c24
AlexStrNik <alex.str.nik@gmail.com>
2019-08-16 02:16:18 +0300

    Add Virtual start button

---
+++ Assets/Objects/VirtualRobbo/VirtualStartButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Varwin;
+using Varwin.Public;
+
+public class VirtualStartButton : MonoBehaviour, IUseStartAware, ITouchStartAware, IUseEndAware, ITouchEndAware
+{
+    public bool StartButtonPressed;
+
+    private Vector3 targetScale;
+    private Vector3 targetPosition;
+
+    private Vector3 normalScale;
+    private Vector3 hoveredScale;
+
+    private Vector3 normalPositon;
+    private Vector3 pressedPosition;
+
+    void Start()
+    {
+        StartButtonPressed = false;
+
+        normalScale = transform.localScale;
+        hoveredScale = new Vector3(normalScale.x * 1.2f, normalScale.y, normalScale.z * 1.2F);
+        targetScale = normalScale;
+
+        normalPositon = transform.localPosition;
+        pressedPosition = new Vector3(normalPositon.x, normalPositon.y - 0.006F, normalPositon.z);
+        targetPosition = normalPositon;
+    }
+
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * 1.7F);
+        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * 1.7F);
+    }
+
+    public void OnTouchStart()
+    {
+        targetScale = hoveredScale;
+    }
+
+    public void OnTouchEnd()
+    {
+        targetScale = normalScale;
+    }
+
+    public void OnUseStart(UsingContext context)
+    {
+        targetPosition = pressedPosition;
+        StartButtonPressed = true;
+    }
+
+    public void OnUseEnd()
+    {
+        targetPosition = normalPositon;
+        StartButtonPressed = false;
+    }
+}
---
+++ Assets/Shaders/Transparent.shader
@@ -0,0 +1,41 @@
+﻿Shader "RobboVarwin/Transparent"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGBA)", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+        LOD 200
+
+        CGPROGRAM
+
+        #pragma surface surf Standard fullforwardshadows alpha
+
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        fixed4 _Color;
+
+        UNITY_INSTANCING_BUFFER_START(Props)
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            // Albedo comes from a texture tinted by color
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            o.Albedo = c.rgb;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
